fd02884665dcf816afac273264c788bc198c05f1
KIRAN <ks@gmail.com>
2025-07-29 19:12:25 +0530

    vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -2,7 +2,7 @@ module "lm-subnets" {
   for_each = var.subnets
   cidr_block = each.value.cidr_block
   source = "./lm-subnets"
-  vpc_id = var.vpc_id
+  vpc_id = var.vpc_id[0]
   env = var.env
   name = each.value.name
 }
--- subntes.tf
+++ subntes.tf
@@ -2,7 +2,6 @@ module "subnets" {
   for_each = var.vpc
   source   = "./subnets"
   subnets = each.value.subnets
-  vpc_id = [for k,v in aws_vpc.main:v.id]
+  vpc_id = [for k,v in aws_vpc.main: v.id]
   env = var.env
-  name = var.name
 }
