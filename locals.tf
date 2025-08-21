1c04ca8fa7f72373491a694a9aa4b2d1fba022fa
KIRAN <ks@gmail.com>
2025-08-21 20:50:39 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -4,4 +4,12 @@ locals{
     ENV     = var.env
     Project = "roboshop"
   }
-}
+  vpc_peering_tags = {
+    Name    = "${var.env}-vpc-to-management-vpc"
+    ENV     = var.env
+    PROJECT = "roboshop"
+  }
+  vpc_ids = [for k, v in aws_vpc.main : v.id]
+
+  }
+
