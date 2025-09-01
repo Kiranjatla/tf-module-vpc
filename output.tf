68dca717365e40a26d118051205e018ce2949673
KIRAN <ks@gmail.com>
2025-09-01 19:04:50 +0530

    subnets

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-#output "subnets" {
-#  value = module.subnets
+##output "subnets" {
+##  value = module.subnets
+##}
+#
+#output "public_subnets" {
+#  value = module.public_subnets
 #}
-
-output "public_subnets" {
-  value = module.public_subnets
-}
-
-output "private_subnets" {
-  value = module.private_subnets
-}
-
-#output "all_private_subnets" {
-#  value = [for k,v in module.private_subnets: v.subnets]
+#
+#output "private_subnets" {
+#  value = module.private_subnets
 #}
+#
+##output "all_private_subnets" {
+##  value = [for k,v in module.private_subnets: v.subnets]
+##}
