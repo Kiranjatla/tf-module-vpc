8d7ba3502bab83c2ba4a750cc18d3481a45ff3fe
KIRAN <ks@gmail.com>
2025-09-01 19:42:02 +0530

    subnets

--- subnets.tf
+++ subnets.tf
@@ -3,7 +3,6 @@ module "private_subnets" {
  source     = "./subnets"
  cidr_block = each.value.cidr_block
  vpc_id     = = aws_vpc.main.id
-main.id
 env = var.env
 name = each.value.name
 subnet_availability_zones = var.subnet_availability_zones
